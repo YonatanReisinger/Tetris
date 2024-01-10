9b2c057b9e6704e24add612eebe4c7c459316920
Danielbar9898 <danielbaron.1808@gmail.com>
2024-01-10 19:50:26 +0200

    Updated shape

--- Tetris/Shape.cpp
+++ Tetris/Shape.cpp
@@ -1,22 +1,40 @@
 #include "Shape.h"
 
+/*input: gets a type that represents the tetronimo type, and a point that repreesents
+the starting point of the tetronimo
+Creates a new shape based on the type and point*/
+Shape::Shape(Type t, Point& start) {
+	shapeType = t;
+	/*switch (t) {
+	case STRAIGHT: {
 
+	}
+	case SQUARE: {
+
+	}
+
+	}*/
+
+}
+/*this function make the shape move down on the board*/
 void Shape::moveDown() {
 	for (int i = 0; i < NUM_OF_POINTS; i++) {
 		points[i].moveDown();
 	}
 }
-
+/*this function make the shape move left on the board*/
 void Shape::moveLeft() {
 	for (int i = 0; i < NUM_OF_POINTS; i++) {
 		points[i].moveLeft();
 	}
 }
-
+/*this function make the shape move right on the board*/
 void Shape::moveRight() {
 	for (int i = 0; i < NUM_OF_POINTS; i++) {
 		points[i].moveRight();
 	}
 }
+
+
 
 
