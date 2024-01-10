9e8d506288763bd42ea586f6882bcbc587a3f7c2
YonatanReisinger <yonatanrisinger33@gmail.com>
2024-01-10 20:43:37 +0200

    a

--- Tetris/Shape.h
+++ Tetris/Shape.h
@@ -8,29 +8,25 @@
 #include <iostream>
 using namespace std;
 // Macros ans enums->
+enum Type { PLUS, SQUARE, STRAIGHT, SKEW, MIRROR_SKEW, L, MIRROR_L };
 #define NUM_OF_POINTS  4
 // <- Macros and enums
 
 class Shape {
 private:
-	/*each shape will be held by an array of 4 points.
-	points [0] is the most left-bottom point
-	points [1] is the most left-top point
-	points [2] is the most right-bottom point
-	points [3] is the most right-top point*/
+	/*each shape will be held by an array of 4 points and an enum that represents
+	the shape type*/
 	Point points[NUM_OF_POINTS];
-	Color shapeColor;
-	char symbol;
-	int x = 4;
+	Type shapeType;
 public:
-	void setColor(Color color);
+	Shape(Type t, Point& start);
+	//void setColor(Color color);
 	void moveDown();
 	void moveLeft();
 	void moveRight();
-	void rotateLeft();
-	void rotateRight();
+	void rotateLeft(Type t);
+	void rotateRight(Type t);
 };
 
 
 #endif //Shape.h
-
