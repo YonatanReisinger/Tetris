86b4c04df2f4423c25c90754073cd086b1b0cd04
YonatanReisinger <yonatanrisinger33@gmail.com>
2024-01-19 23:29:00 +0200

    ?

--- Tetris/Point.h
+++ Tetris/Point.h
@@ -9,7 +9,6 @@ using namespace std;
 // <- Imports
 
 // Macros and enums ->
-enum class Color {WHITE = (FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE),RED  = BACKGROUND_RED,BLUE = BACKGROUND_BLUE,GREEN = BACKGROUND_GREEN,BRWON = RED|GREEN,PURPLE = 80,GREY = 143,CYAN = 183};
 // <- Macros and enums
 
 class Point {
@@ -17,10 +16,9 @@ private:
 	short int x;
 	short int y;
 	char symbol;
-	Color color;
 	//Color color;
 public:
-	Point(short int x = 0, short int y = 0, char symbol = EMPTY,Color color = Color::WHITE);
+	Point(short int x = 0, short int y = 0, char symbol = EMPTY);
 	bool setX(short int x);
 	short int getX() const;
 	bool setY(short int y);
@@ -38,8 +36,7 @@ public:
 	bool moveRight();
 	bool move(Directions direction);
 	bool areCoordinatesEqual(const Point& other) const;
-	inline Color getColor();
-	bool setColor(Color color);
+
 	friend class Board;
 };
 #endif //Point.h
